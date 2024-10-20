c1844211d83b02484aebaff9dcf6bdcd5a042a78
zww <335218152@qq.com>
2024-10-21 01:09:23 +0800

    2

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <head>
+          <title>My App</title>
+        </head>
+        <body>{children}</body>
+      </html>
+    );
+  }
+  
